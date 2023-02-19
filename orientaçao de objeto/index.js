d9b47e5ddae00e5dcf6a706dbb5353fea4b2fa2a
Joel Santos <joeltere9@gmail.com>
2023-02-19 13:28:05 -0300

    joel

---
+++ orientaçao de objeto/index.js
@@ -0,0 +1,23 @@
+const cliente1Nome = 'Joel';
+const clienteCpf = '01191269582'
+const cliente1Agencia = 1001
+const cliente1Saldo = 0
+
+
+
+class Cliente {
+    nome;
+      cpf;
+     agencia;
+     saldo;
+}
+
+const cliente1 = new Cliente()
+
+
+cliente1.nome = 'Joel';
+cliente1.cpf = '4444';
+cliente1.agencia  = 'itau';
+cliente1.saldo= 0;
+
+console.log(cliente1)
