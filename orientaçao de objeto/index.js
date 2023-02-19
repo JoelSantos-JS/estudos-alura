a85eb6b32b24f5a5c1f3c5188617d341d04da416
Joel Santos <joeltere9@gmail.com>
2023-02-19 15:11:22 -0300

    d

--- orientaçao de objeto/index.js
+++ orientaçao de objeto/index.js
@@ -8,16 +8,43 @@ const cliente1Saldo = 0
 class Cliente {
     nome;
       cpf;
-     agencia;
-     saldo;
+   
 }
 
-const cliente1 = new Cliente()
+class ContaCorrente  {
+   
+  agencia;
+     saldo;
+     sacar(valor) {
+      if(this.saldo >= valor) {
+        this.saldo -= valor
+      }
+     }
 
+     depositar(valor) {
+      if(valor > 0){
+      this.saldo += valor
+
+      }
+     }
+
+};
+
+
+
+const cliente1 = new Cliente()
+const conta = new ContaCorrente()
 
 cliente1.nome = 'Joel';
 cliente1.cpf = '4444';
 cliente1.agencia  = 'itau';
 cliente1.saldo= 0;
 
-console.log(cliente1)
+
+conta.agencia = 65564,
+conta.saldo = 45,
+conta.depositar(200)
+conta.depositar(400)
+conta.sacar(500)
+
+console.log(conta.saldo)
